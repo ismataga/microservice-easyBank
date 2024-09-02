6c7e566213efced145b99ac5d305252b1b20a204
ismet-aghamirzayev <ismataga1@gmail.com>
2024-09-02 10:55:51 +0400

    account ms

---
+++ section2/accounts/src/main/java/com/ismataga/accounts/entity/Accounts.java
@@ -0,0 +1,29 @@
+package com.ismataga.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Accounts extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+
+    @Column(name = "branch_Address")
+    private String branchAddress;
+}
