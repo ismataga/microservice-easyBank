1d9600078400efc3bf415f53f80aa217639104d4
ismet-aghamirzayev <ismataga1@gmail.com>
2024-09-09 14:48:09 +0400

    Add mysql docker compose

---
+++ section4/accounts/src/main/java/com/ismataga/accounts/AccountsApplication.java
@@ -0,0 +1,39 @@
+package com.ismataga.accounts;
+
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts Management System",
+                version = "1.0",
+                description = "API for managing accounts and customers.",
+                contact = @Contact(
+                        name = "Ismat Aghamirzayev",
+                        email = "ismataga.aghamirzayev@gmail.com",
+                        url = "https://github.com/ismataga/accounts-management-system"
+                ),
+                license = @io.swagger.v3.oas.annotations.info.License(
+                        name = "MIT License",
+                        url = "https://opensource.org/licenses/MIT"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Find more info here",
+                url = "https://swagger.io"
+        )
+)
+public class AccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
+
+}
